477bee34f7f5934a21d32db614310008f09d3150
kaitokimuraofficial <k.kaito0422japan@gmail.com>
2025-04-30 18:05:56 +0900

    Create ECS Cluster

--- ecs.tf
+++ ecs.tf
@@ -3,3 +3,22 @@ resource "aws_service_discovery_http_namespace" "daily_report_system" {
   description = "The namespace of development environment"
 }
 
+resource "aws_ecs_cluster" "main" {
+  name = "main-${local.name_suffix}"
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.symmetric.arn
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.daily_report_system.name
+      }
+    }
+  }
+
+  service_connect_defaults {
+    namespace = aws_service_discovery_http_namespace.daily_report_system.arn
+  }
+}
