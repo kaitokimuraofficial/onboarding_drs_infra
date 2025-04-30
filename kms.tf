6a6cc1a4bf251d26425451575d352b7c945f82bc
kaitokimuraofficial <k.kaito0422japan@gmail.com>
2025-04-30 16:53:18 +0900

    Update KMS policy to use on ECR

---
+++ kms.tf
@@ -0,0 +1,40 @@
+resource "aws_kms_key" "symmetric" {
+  description = "Symmetric encryption KMS key"
+}
+
+resource "aws_kms_alias" "symmetric_alias" {
+  name          = "alias/symmetric-${local.name_suffix}"
+  target_key_id = aws_kms_key.symmetric.key_id
+}
+
+resource "aws_kms_key_policy" "symmetric" {
+  key_id = aws_kms_key.symmetric.id
+  policy = jsonencode({
+    Statement = [
+      {
+        Sid    = "Enable only myself to do everything"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.kk_iam_user_arn}"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow ECR to use the key"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecr.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "environment" {
   type        = string
 }
 
+variable "kk_iam_user_arn" {
+  description = "The ARN of kk"
+  type        = string
+}
+
 variable "s3_backend_bucket_name" {
   description = "S3 backend bucket name"
   type        = string
