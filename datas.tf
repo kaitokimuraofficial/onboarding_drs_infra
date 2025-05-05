b979e63b10de902768d7f22b4a8b2bf215823d7d
kaitokimuraofficial <k.kaito0422japan@gmail.com>
2025-05-05 09:24:06 +0900

    Add new policy to use ECS Exec

--- datas.tf
+++ datas.tf
@@ -1,1 +1,57 @@
+data "aws_iam_policy_document" "ecs_task_exec" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "ssm:GetParameters"
+    ]
+    resources = [
+      "arn:aws:secretsmanager:${var.aws_region}:${var.kk_account_id}:secret:prod/drs/*"
+    ]
+  }
 
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:DescribeLogGroups"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:${var.aws_region}:${var.kk_account_id}:log-group:/aws/ecs/${aws_cloudwatch_log_group.daily_report_system.name}:*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
