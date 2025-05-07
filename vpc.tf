b620537aa961e8d358e3dd825e3f9b3cb54b603e
kaitokimuraofficial <k.kaito0422japan@gmail.com>
2025-05-08 07:29:41 +0900

    Create Gateway VPC Endpoint

--- vpc.tf
+++ vpc.tf
@@ -61,14 +61,14 @@ resource "aws_route_table" "public" {
     Name = "public-${local.name_suffix}"
   }
 }
-/*
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "private-${local.name_suffix}"
   }
 }
-*/
+
 resource "aws_route_table_association" "public" {
   for_each = aws_subnet.public_1a
 
@@ -81,14 +81,11 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
-/*
-resource "aws_route_table_association" "private" {
-  for_each = aws_subnet.private
 
-  subnet_id      = each.value.id
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_1a["ecs"].id
   route_table_id = aws_route_table.private.id
 }
-*/
 
 resource "aws_security_group" "interface" {
   vpc_id = aws_vpc.main.id
@@ -120,6 +117,38 @@ resource "aws_vpc_endpoint" "interface" {
   tags = {
     Name = "${each.key}-${local.name_suffix}"
   }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id = aws_vpc.main.id
+
+  vpc_endpoint_type = "Gateway"
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+
+  route_table_ids = [aws_route_table.private.id]
+
+  tags = {
+    Name = "s3-${local.name_suffix}"
+  }
+}
+
+resource "aws_vpc_endpoint_policy" "s3" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "*"
+        },
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
 }
 
 /*
