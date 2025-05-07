ba6ad9172f79bf8c728fdf845389ff52e149fe31
kaitokimuraofficial <k.kaito0422japan@gmail.com>
2025-05-08 00:15:41 +0900

    Create DB instance

--- rds.tf
+++ rds.tf
@@ -1,25 +1,21 @@
-/*
 resource "aws_db_subnet_group" "mysql" {
   name = "mysql-${local.name_suffix}"
 
   subnet_ids = [
-    aws_subnet.private["private-ne-1a"].id,
-    aws_subnet.private["private-ne-1c"].id
+    aws_subnet.private_1a["db"].id,
+    aws_subnet.private_1c["db"].id
   ]
 }
 
 resource "aws_security_group" "mysql" {
-  description = "For MySQL DB instance"
-  vpc_id      = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port = 3306
     to_port   = 3306
     protocol  = "tcp"
     cidr_blocks = [
-      aws_subnet.public["public-ne-1a"].cidr_block,
-      aws_subnet.private["private-ne-1a"].cidr_block,
-      aws_subnet.private["private-ne-1c"].cidr_block
+      aws_subnet.public_1a["bastion"].cidr_block
     ]
   }
 }
@@ -34,9 +30,11 @@ resource "aws_db_instance" "mysql" {
   manage_master_user_password   = true
   master_user_secret_kms_key_id = aws_kms_key.symmetric.key_id
   db_subnet_group_name          = aws_db_subnet_group.mysql.name
-  vpc_security_group_ids        = [aws_security_group.mysql.id]
   multi_az                      = false
   auto_minor_version_upgrade    = false
+
+  vpc_security_group_ids = [
+    aws_security_group.mysql.id
+  ]
 }
-*/
 
