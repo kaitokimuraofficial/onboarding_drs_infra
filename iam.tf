c4b8c1a775672197383621a70372205b1a386a1d
kaitokimuraofficial <k.kaito0422japan@gmail.com>
2025-05-08 01:22:26 +0900

    Add GHA assume policy and role

--- iam.tf
+++ iam.tf
@@ -1,4 +1,3 @@
-/*
 resource "aws_iam_role" "drs_deploy_gha" {
   name               = "drs-deploy-gha-${local.name_suffix}"
   assume_role_policy = data.aws_iam_policy_document.drs_deploy_gha_assume.json
@@ -8,7 +7,7 @@ resource "aws_iam_role_policy" "drs_deploy_gha" {
   role   = aws_iam_role.drs_deploy_gha.id
   policy = data.aws_iam_policy_document.drs_deploy_gha.json
 }
-
+/*
 resource "aws_iam_role" "ecs_task_exec" {
   name               = "ecs-task-exec-${local.name_suffix}"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_exec_assume.json
